ae44c38be8ffbb8d75abb615fe988614357d93bd
zedfeng <zf232@cam.ac.uk>
2016-03-07 00:06:12 +0000

    commit

--- OJ/LC_Longest Consecutive Sequence.cpp
+++ OJ/LC_Longest Consecutive Sequence.cpp
@@ -1,9 +1,32 @@
 #include<vector>
+#include<unordered_map>
 using namespace std;
 
 class Solution {
 public:
 	int longestConsecutive(vector<int>& nums) {
-
+		unordered_map<int, bool> nums_map;
+		for (auto i : nums) {
+			nums_map[i] = false;
+		}
+		int result = 0;
+		for (auto i : nums) {
+			if (nums_map[i] == true) {
+				continue;
+			}
+			int len = 1;
+			nums_map[i] = true;
+			//search from the consecutive elements of i
+			for (int j = i + 1; nums_map.find(j) != nums_map.end(); j++) {
+				nums_map[j] = true;
+				len++;
+			}
+			for (int j = i - 1; nums_map.find(j) != nums_map.end(); j--) {
+				nums_map[j] = true;
+				len++;
+			}
+			result = result > len ? result : len;
+		}
+		return result;
 	}
 };
