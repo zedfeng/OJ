df2dd71c4aa2455791f3c822c062b7f253e27c6e
zedfeng <zf232@cam.ac.uk>
2016-03-08 19:48:41 +0000

    commit

--- OJ/LC_Gray Code.cpp
+++ OJ/LC_Gray Code.cpp
@@ -1,19 +1,23 @@
-#include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
+
 class Solution {
 public:
 	vector<int> grayCode(int n) {
 		vector<int> result;
-		const size_t size = 1 << n;
-		result.reserve(size);
-		for (size_t i = 0; i < size; ++i){
-			result.push_back(binary_to_gray(i));
+		if (n == 0) {
+			result.push_back(0);
+		}
+		if (n > 0) {
+			vector<int> half = grayCode(n - 1);
+			result.insert(result.end(), half.begin(), half.end());
+			reverse(half.begin(), half.end());
+			int base = 1 << n - 1;
+			for (int i = 0; i < half.size(); i++) {
+				result.push_back(half[i] + base);
+			}
 		}
 		return result;
-	}
-private:
-	unsigned int binary_to_gray(unsigned int n){
-		return n ^ (n >> 1);
 	}
 };
--- OJ/LC_Set Matrix Zeroes.cpp
+++ OJ/LC_Set Matrix Zeroes.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
 #include<vector>
 using namespace std;
+
 class Solution {
 public:
 	void setZeroes(vector<vector<int>>& matrix) {
@@ -8,42 +8,40 @@ public:
 		const size_t n = matrix[0].size();
 		bool first_row_has_zero = false;
 		bool first_col_has_zero = false;
-		for (size_t i = 0; i < n; i++){
-			if (matrix[0][i] == 0){
+		for (int j = 0; j < n; j++) {
+			if (matrix[0][j] == 0) {
 				first_row_has_zero = true;
 				break;
 			}
 		}
-		for (size_t i = 0; i < m; i++){
-			if (matrix[i][0] == 0){
+		for (int i = 0; i < m; i++) {
+			if (matrix[i][0] == 0) {
 				first_col_has_zero = true;
 				break;
 			}
 		}
-		for (size_t i = 0; i < m; i++){
-			for (size_t j = 0; j < n; j++){
-				if (matrix[i][j] == 0){
-					//标记需要最后设0的行列号
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (matrix[i][j] == 0) {
 					matrix[0][j] = 0;
 					matrix[i][0] = 0;
 				}
 			}
 		}
-		//除了第一行第一列，
-		for (size_t i = 1; i < m; i++){
-			for (int j = 1; j < n; j++){
-				if (matrix[i][0] == 0 || matrix[0][j] == 0){
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				if (matrix[i][0] == 0 || matrix[0][j] == 0) {
 					matrix[i][j] = 0;
 				}
 			}
 		}
-		if (first_row_has_zero){
-			for (size_t i = 0; i < n; i++){
-				matrix[0][i] = 0;
+		if (first_row_has_zero) {
+			for (int j = 0; j < n; j++) {
+				matrix[0][j] = 0;
 			}
 		}
-		if (first_col_has_zero){
-			for (size_t i = 0; i < m; i++){
+		if (first_col_has_zero) {
+			for (int i = 0; i < m; i++) {
 				matrix[i][0] = 0;
 			}
 		}
