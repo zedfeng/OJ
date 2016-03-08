47aece52f09e4d311ccc2785a160ba325d1ecf6f
zedfeng <zf232@cam.ac.uk>
2016-03-08 15:46:43 +0000

    commit

--- OJ/LC_Plus One.cpp
+++ OJ/LC_Plus One.cpp
@@ -1,22 +1,17 @@
-#include<iostream>
 #include<vector>
 using namespace std;
+
 class Solution {
 public:
 	vector<int> plusOne(vector<int>& digits) {
 		int carry = 1;
-		for (int i = digits.size() - 1; i >= 0; i--){
-			if (digits[i] + carry >= 10){
-				digits[i] = (digits[i] + carry) % 10;
-				carry = 1;
-			}
-			else{
-				digits[i] += carry;
-				carry = 0;
-			}
-			if (carry == 1 && i == 0){
-				digits.insert(digits.begin(), 1);
-			}
+		for (auto i = digits.rbegin(); i != digits.rend(); i++) {
+			*i += carry;
+			carry = *i / 10;
+			*i %= 10;
+		}
+		if (carry) {
+			digits.insert(digits.begin(), 1);
 		}
 		return digits;
 	}
--- OJ/LC_Trapping Rain Water.cpp
+++ OJ/LC_Trapping Rain Water.cpp
@@ -1,11 +1,26 @@
-#include<iostream>
-#include<stack>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 class Solution {
 public:
 	int trap(vector<int>& height) {
-
+		int water = 0;
+		vector<int> left_max_height(height.size(), 0);
+		vector<int> right_max_height(height.size(), 0);
+		int left_max = 0, right_max = 0;
+		//scan from left to right and record the left maximum height of bar i.
+		for (int i = 0; i < height.size(); i++) {
+			left_max_height[i] = i == 0 ? 0 : max(left_max_height[i - 1], height[i - 1]);
+		}
+		//scan from right to left and record the right maximum height of bar i.
+		for (int i = height.size() - 1; i >= 0; i--) {
+			right_max_height[i] = i == height.size() - 1 ? 0 : max(right_max_height[i + 1], height[i + 1]);
+		}
+		//get the water of each bar
+		for (int i = 0; i < height.size(); i++) {
+			water += max(min(left_max_height[i], right_max_height[i]) - height[i], 0);
+		}
+		return water;
 	}
 };
