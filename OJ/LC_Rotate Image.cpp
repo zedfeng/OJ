e23bf6973b8300a7bf2a9b1c35a1c5a3e4e23e02
zedfeng <zf232@cam.ac.uk>
2016-03-08 12:54:18 +0000

    commit

--- OJ/LC_Rotate Image.cpp
+++ OJ/LC_Rotate Image.cpp
@@ -1,19 +1,19 @@
-#include<iostream>
 #include<vector>
 #include<algorithm>
 using namespace std;
+
 class Solution {
 public:
 	void rotate(vector<vector<int>>& matrix) {
 		int n = matrix.size();
-		for (int i = 0; i < n - 1; i++){
-			for (int j = 0; j < n - i - 1; j++){
-				swap(matrix[i][j], matrix[n - 1 - j][n - 1 - i]);
+		for (int i = 0, j = n - 1; i < j; i++, j--) {
+			for (int k = 0; k < n; k++) {
+				swap(matrix[i][k], matrix[j][k]);
 			}
 		}
-		for (int i = 0; i < n / 2; i++){
-			for (int j = 0; j < n; j++){
-				swap(matrix[i][j], matrix[n - 1 - i][j]);
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				swap(matrix[i][j], matrix[j][i]);
 			}
 		}
 	}
