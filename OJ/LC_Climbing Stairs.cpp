b4f18cb831af6805229cb1de6eee33e7a54cd3eb
zedfeng <zf232@cam.ac.uk>
2016-03-08 15:53:53 +0000

    commit

--- OJ/LC_Climbing Stairs.cpp
+++ OJ/LC_Climbing Stairs.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
 using namespace std;
+
 class Solution {
 public:
 	int climbStairs(int n) {
-		//斐波那契数列递推公式
+		//Fibonacci
 		int prev = 1;
 		int pprev = 1;
 		int curr = 1;
-		for (int i = 2; i <= n; i++){
+		for (int i = 2; i <= n; i++) {
 			curr = prev + pprev;
 			pprev = prev;
 			prev = curr;
