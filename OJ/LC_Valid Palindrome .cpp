bd97d3544181806ecca326af30ad56e0bbee6fb1
zedfeng <zf232@cam.ac.uk>
2016-03-10 12:29:17 +0000

    commit

--- OJ/LC_Valid Palindrome .cpp
+++ OJ/LC_Valid Palindrome .cpp
@@ -1,24 +1,25 @@
 #include<string>
 #include<algorithm>
 using namespace std;
+
 class Solution {
 public:
 	bool isPalindrome(string s) {
 		transform(s.begin(), s.end(), s.begin(), ::tolower);
 		auto left = s.begin(), right = prev(s.end());
-		while (left < right){
-			if (!isalnum(*left)){
+		while (left < right) {
+			if (!isalnum(*left)) {
 				left++;
 			}
-			else if (!isalnum(*right)){
+			else if (!isalnum(*right)) {
 				right--;
 			}
-			else if (*left != *right){
+			else if (*left == *right) {
+				left++;
+				right--;
+			}
+			else {
 				return false;
-			}
-			else{
-				left++;
-				right--;
 			}
 		}
 		return true;
