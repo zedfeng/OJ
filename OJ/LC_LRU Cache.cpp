88effb049fe7890cd20442e5b9ba120816f47bff
zedfeng <zf232@cam.ac.uk>
2016-03-10 12:18:06 +0000

    commit

--- OJ/LC_LRU Cache.cpp
+++ OJ/LC_LRU Cache.cpp
@@ -1,19 +1,16 @@
 #include<list>
 #include<unordered_map>
 using namespace std;
-class LRUCache{
+
+class LRUCache {
 public:
-	struct CacheNode{
-		int key;
-		int val;
-		CacheNode(int k, int v) :key(k), val(v) {}
-	};
 	LRUCache(int capacity) {
 		this->capacity = capacity;
 	}
 
 	int get(int key) {
-		if (cache_map.find(key) == cache_map.end()){
+		auto result = cache_map.find(key);
+		if (result == cache_map.end()) {
 			return -1;
 		}
 		cache_list.splice(cache_list.begin(), cache_list, cache_map[key]);
@@ -22,21 +19,26 @@ public:
 	}
 
 	void set(int key, int value) {
-		if (cache_map.find(key) == cache_map.end()){
-			if (cache_map.size() == capacity){
+		if (get(key) != -1) {
+			cache_map[key]->val = value;
+			cache_list.splice(cache_list.begin(), cache_list, cache_map[key]);
+			cache_map[key] = cache_list.begin();
+		}
+		else {
+			if (cache_list.size() == capacity) {
 				cache_map.erase(cache_list.back().key);
 				cache_list.pop_back();
 			}
 			cache_list.push_front(CacheNode(key, value));
 			cache_map[key] = cache_list.begin();
 		}
-		else{
-			cache_map[key]->val = value;
-			cache_list.splice(cache_list.begin(), cache_list, cache_map[key]);
-			cache_map[key] = cache_list.begin();
-		}
 	}
 private:
+	struct CacheNode {
+		int key;
+		int val;
+		CacheNode(int k, int v) :key(k), val(v) {};
+	};
 	int capacity;
 	list<CacheNode> cache_list;
 	unordered_map<int, list<CacheNode>::iterator> cache_map;
