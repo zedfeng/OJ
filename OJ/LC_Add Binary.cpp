bce242b4ba1b059becbee319375576706c511722
zedfeng <zf232@cam.ac.uk>
2016-03-10 12:37:41 +0000

    commit

--- OJ/LC_Add Binary.cpp
+++ OJ/LC_Add Binary.cpp
@@ -1,5 +1,6 @@
 #include<string>
 using namespace std;
+
 class Solution {
 public:
 	string addBinary(string a, string b) {
@@ -8,20 +9,15 @@ public:
 		reverse(a.begin(), a.end());
 		reverse(b.begin(), b.end());
 		int carry = 0;
-		for (int i = 0; i < len; i++){
+		for (int i = 0; i < len; i++) {
 			int a_num = i < a.length() ? a[i] - '0' : 0;
 			int b_num = i < b.length() ? b[i] - '0' : 0;
 			int sum = a_num + b_num + carry;
-			if (sum >= 2){
-				sum %= 2;
-				carry = 1;
-			}
-			else{
-				carry = 0;
-			}
+			carry = sum / 2;
+			sum %= 2;
 			result.insert(result.begin(), sum + '0');
 		}
-		if (carry){
+		if (carry) {
 			result.insert(result.begin(), carry + '0');
 		}
 		return result;
