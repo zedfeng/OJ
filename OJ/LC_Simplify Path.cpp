572738dfd210ee2ae66e09f7b30bae6db840f7e9
zedfeng <zf232@cam.ac.uk>
2016-03-16 11:28:15 +0000

    commit

--- OJ/LC_Simplify Path.cpp
+++ OJ/LC_Simplify Path.cpp
@@ -1,9 +1,37 @@
 #include<string>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 class Solution {
 public:
 	string simplifyPath(string path) {
-
+		vector<string> dirs;
+		for (auto i = path.begin(); i < path.end(); i++) {
+			auto j = find(i, path.end(), '/');
+			auto dir = string(i, j);
+			if (!dir.empty() && dir != ".") {
+				if (dir == "..") {
+					if (!dirs.empty()) {
+						dirs.pop_back();
+					}
+				}
+				else {
+					dirs.push_back(dir);
+				}
+			}
+			i = j;
+		}
+		string result;
+		if (dirs.empty()) {
+			result.push_back('/');
+		}
+		else {
+			for (auto dir : dirs) {
+				result.push_back('/');
+				result.append(dir);
+			}
+		}
+		return result;
 	}
 };
