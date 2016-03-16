7bdc122b8478da27baf86bb280bf228861d50fe6
zedfeng <zf232@cam.ac.uk>
2016-03-16 15:36:22 +0000

    commit

--- OJ/LC_Regular Expression Matching.cpp
+++ OJ/LC_Regular Expression Matching.cpp
@@ -3,7 +3,22 @@ using namespace std;
 
 class Solution {
 public:
+	bool recursiveIsMatch(string::iterator s, string::iterator p) {
+		if (*p == '\0') {
+			return *s == '\0';
+		}
+		if (*(p + 1) != '*') {
+			return ((*p == *s) || (*p == '.'&&*s != '\0')) && recursiveIsMatch(s + 1, p + 1);
+		}
+		while ((*p == *s) || (*p == '.'&&*s != '\0')) {
+			if (recursiveIsMatch(s, p + 2)) {
+				return true;
+			}
+			s++;
+		}
+		return recursiveIsMatch(s, p + 2);
+	}
 	bool isMatch(string s, string p) {
-
+		return recursiveIsMatch(s.begin(), p.begin());
 	}
 };
--- OJ/LC_Wildcard Matching.cpp
+++ OJ/LC_Wildcard Matching.cpp
@@ -1,9 +1,36 @@
 #include<string>
+#include<vector>
 using namespace std;
 
 class Solution {
 public:
 	bool isMatch(string s, string p) {
-
+		auto s_it = s.begin();
+		auto p_it = p.begin();
+		auto s_star = s.end();
+		auto p_star = p.end();
+		while (*s_it) {
+			if (*s_it == *p_it || *p_it == '?') {
+				++s_it;
+				++p_it;
+				continue;
+			}
+			if (*p_it == '*') {
+				s_star = s_it;
+				p_star = p_it;
+				++p_it;
+				continue;
+			}
+			if (*p_star) {
+				p_it = p_star + 1;
+				s_it = ++s_star;
+				continue;
+			}
+			return false;
+		}
+		while (*p_it == '*') {
+			++p_it;
+		}
+		return !*p_it;
 	}
 };
