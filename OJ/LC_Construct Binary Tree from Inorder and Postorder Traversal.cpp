0c84ac2618a4cef27ad09e42225ae3be9ad78767
Ziqiang Feng <ziqiang.nicholas.feng@gmail.com>
2016-10-09 20:56:51 +0100

    new

---
+++ OJ/LC_Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -0,0 +1,15 @@
+#include<vector>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+	TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+
+	}
+};
